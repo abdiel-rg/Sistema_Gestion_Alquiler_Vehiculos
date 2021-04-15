d0c7cb4dd76efa8cf7fa8fdc3930c0c650677bab
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-04-15 15:26:10 -0400

    Setup Database connection
    • Add Context to Startup
    • Add DB connection string
    • Add needed NuGet packages
    • Update gitignore

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
+using Sistema_Gestion_Alquiler_Vehiculos.Areas.Identity;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
@@ -9,7 +10,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Sistema_Gestion_Alquiler_Vehiculos.Areas.Identity;
 using Sistema_Gestion_Alquiler_Vehiculos.Data;
 using System;
 using System.Collections.Generic;
@@ -31,16 +31,19 @@ namespace Sistema_Gestion_Alquiler_Vehiculos
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDbContext<SGAVContext>(options =>
+            {
+                options.UseSqlite(
+                    Configuration["ConnectionStrings:DefaultConnection"]
+                    );
+            });
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    // RequireConfirmedAccount set to false because Mail Service hasn't been implemented yet
+                    .AddEntityFrameworkStores<SGAVContext>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
